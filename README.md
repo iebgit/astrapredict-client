50ba32c9ba2149d8a3b73f0a9993d039ea11843e
iebgit <iebdev84@gmail.com>
2023-04-11 08:48:17 -0400

    add images and update Readme

--- README.md
+++ README.md
@@ -1,47 +1,19 @@
-# Getting Started with Create React App
+# ⭐ Astra Predict
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+![My Image](./src/assets/screen.png)
 
-## Available Scripts
+General overview:
+![My Image](./src/assets/schema.png)
 
-In the project directory, you can run:
+AstraPredict attempts to answer an ancient question:
+Can the stars predict the future?
 
-### `yarn start`
+This project sets out to answer this question using machine learning to train models with historical data to see if any useful predictions can be made using the position of the planets in constellations.
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+The first implementation of this process is the prediction of the price of bitcoin.
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+The client is a simple responsive react app that returns data from the api. In a local environement, with the server running, you can train and test the model by feeding it data from the coingecko api and the planet positions data.
 
-### `yarn test`
+- A version is deployed at: https://astrapredict.vercel.app
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `yarn build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `yarn eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-# astrapredict-client
+- The links provided may be slow due to using free servicesa
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,206 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Box,
+  SimpleGrid,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableCaption,
+  TableContainer,
+} from "@chakra-ui/react";
+const regexPattern = /[^A-Za-z]/g;
+
+interface IData {
+  data: {
+    planets: Array<any>
+    location: {city: String, ip: String, region: String, country: String, time: String},
+    btc: any,
+    predicted: any
+  }
+}
+
+const default_data = {data: {planets: [], location: {city: "", ip: "", region: "", country: "", time: ""}, btc: "", predicted: ""}}
 
 function App() {
+  const [res, setRes] = useState<IData>(default_data);
+  const [images, setImages] = useState(
+    importAll(require.context("./assets", false, /\.(png|jpe?g|svg)$/))
+  );
+
+  useEffect(() => {
+    const getSidereal = async () => {
+      const data: any = await axios.get(`http://localhost:5000/predict`);
+      setRes(data);
+    };
+    getSidereal();
+  }, []);
+
+  function importAll(r: any) {
+    let imgs: any = {};
+    r.keys().forEach((item: String) => {
+      imgs[item.replace("./", "")] = r(item);
+    });
+    return imgs;
+  }
+
+  function getImage() {
+    return images[
+      `${res.data.planets[0].longitude
+        .replace(regexPattern, "")
+        .toLowerCase()}${getRandomInt(4)}.png`
+    ];
+  }
+
+  function getRandomInt(max: number) {
+    return Math.floor(Math.random() * max);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      {res?.data?.btc ? (
+        <SimpleGrid minChildWidth="400px" columns={2} spacing={4}>
+          <Box >
+            {!!res?.data?.btc && (
+              <center>
+                <h3>
+                  <strong style={{ color: "#FCB13B" }}>
+                    {res?.data.location.city}, {res?.data.location.region}
+                  </strong>
+                  <br />
+                  <small>{res?.data.location.time}</small>
+                </h3>
+                <br />
+                <div style={{ justifyContent: "space-between" }}>
+                  {images ? (
+                    <img
+                      style={{ borderRadius: "50%", maxWidth: "50%" }}
+                      src={getImage()}
+                      alt={res?.data.planets[0].planet}
+                    ></img>
+                  ) : (
+                    <div className="loader">
+                      <div className="outer"></div>
+                      <div className="inner"></div>
+                    </div>
+                  )}
+                </div>
+                <br />
+                <p
+                  style={{
+                    color: "#FF6630",
+                    fontWeight: "bold",
+                    fontSize: "large",
+                  }}
+                >
+                  {res?.data.planets[0].planet.toUpperCase()}:{" "}
+                  {res?.data.planets[0].longitude}
+                </p>
+                <br />
+                <TableContainer
+                  style={{ fontSize: "small", fontWeight: "bold" }}
+                >
+                  <Table>
+                    <TableCaption style={{ color: "white" }}>
+                      Model Prediction vs Live Price
+                    </TableCaption>
+                    <Thead>
+                      <Tr>
+                        <Th style={{ color: "white" }}>Description</Th>
+                        <Th style={{ color: "white" }}>Value</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      <Tr>
+                        <Td>Predicted 24 Hr % △ BTC </Td>
+                        <Td>
+                          {" "}
+                          <span
+                            style={{ color: "#FCB13B", fontWeight: "bold" }}
+                          >
+                            {Math.round(res?.data.predicted * 100)} %
+                          </span>
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>Predicted </Td>
+                        <Td>
+                          {" "}
+                          <span
+                            style={{ color: "#FCB13B", fontWeight: "bold" }}
+                          >
+                            $
+                            {Math.round(
+                              res?.data.btc +
+                                res?.data.btc * res?.data.predicted
+                            )}
+                          </span>
+                        </Td>
+                      </Tr>
+                      <Tr>
+                        <Td>Current </Td>
+                        <Td>
+                          {" "}
+                          <span
+                            style={{ color: "#FCB13B", fontWeight: "bold" }}
+                          >
+                            ${res?.data.btc}
+                          </span>
+                        </Td>
+                      </Tr>
+                    </Tbody>
+                  </Table>
+                </TableContainer>
+              </center>
+            )}
+          </Box>
+          <Box>
+            <center style={{ fontSize: "small", fontWeight: "bold" }}>
+              <TableContainer>
+                <Table>
+                  <TableCaption style={{ color: "white" }}>
+                    Planet Positions
+                  </TableCaption>
+                  <Thead>
+                    <Tr>
+                      <Th style={{ color: "white" }}>Planets</Th>
+                      <Th style={{ color: "white" }}>Positions</Th>
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {res?.data.planets.slice(1).map((planet, i) => {
+                      return (
+                        <Tr key={i}>
+                          <Td>{planet.planet}</Td>
+                          <Td>
+                            {" "}
+                            <span
+                              style={{ color: "#FCB13B", fontWeight: "bold" }}
+                            >
+                              {planet.longitude}
+                            </span>
+                          </Td>
+                        </Tr>
+                      );
+                    })}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </center>
+          </Box>
+        </SimpleGrid>
+      ) : (
+        <div className="App-center">
+          <div className="loader">
+            <div className="outer"></div>
+            <div className="inner"></div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
