43b9df4b140ea784206989726433b9706d0c1853
iebgit <iebdev84@gmail.com>
2025-01-28 12:25:43 -0500

    Add message to loader letting user know the process can take some time to complete.

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,15 @@
 import "../App.css";
+import {Text, useColorModeValue} from "@chakra-ui/react"
 
 export default function Loader() {
   return (
     <div className="App-center">
+      <Text color={useColorModeValue("orange.200", "orange.500")}>
+        <center style={{padding: "5px"}}>We are currently using free servers.</center>
+        <center style={{padding: "5px"}}>This may take a while...</center>
+      </Text>
+     
+      <br/>
       <div className="loader">
         <div className="outer"></div>
         <div className="inner"></div>
